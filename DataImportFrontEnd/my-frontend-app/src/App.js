9debfdc594c3face5ccd4afd5c2a5fdee6c90d39
Emma Holt <emma.holt@omnifederal.com>
2025-06-30 11:41:12 -0500

    DataImportFrontEnd

---
+++ DataImportFrontEnd/my-frontend-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QueryForm from './QueryForm.js';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <QueryForm />
+      </header>
+    </div>
+  );
+}
