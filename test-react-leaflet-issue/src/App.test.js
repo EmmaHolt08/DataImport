f4ed78623688acd2a3a26f669af00910b1b9ba2a
Emma Holt <emma.holt@omnifederal.com>
2025-07-24 14:25:41 -0500

    test in json updated?

--- test-react-leaflet-issue/src/App.test.js
+++ test-react-leaflet-issue/src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import AuthPage from './AuthPage'; // Make sure this path is correct relative to App.js
+
+const MockApp = () => {
+  return (
+    <BrowserRouter>
+      <AuthPage>
+        {/* This content will be rendered if the user is authenticated */}
+        <div data-testid="app-content">Welcome to the Landslide App!</div>
+      </AuthPage>
+    </BrowserRouter>
+  );
+};
+
+
+describe('AuthPage component', () => {
+  test('renders Landslide Report Login when not authenticated', () => {
+    render(<MockApp />);
+
+    const loginTitle = screen.getByText(/Landslide Report Login/i);
+    expect(loginTitle).toBeInTheDocument();
+
+    const appContent = screen.queryByTestId('app-content');
+    expect(appContent).not.toBeInTheDocument();
+  });
 });
