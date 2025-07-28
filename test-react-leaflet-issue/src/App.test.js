50f4870def391e5aab4f6d38817e28d0dbb245ec
Emma Holt <emma.holt@omnifederal.com>
2025-07-28 11:19:40 -0500

    added to json

--- test-react-leaflet-issue/src/App.test.js
+++ test-react-leaflet-issue/src/App.test.js
@@ -1,19 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-//import { BrowserRouter } from 'react-router-dom'; 
 
-import ReportForm from './ReportForm'; // Make sure this path is correct relative to App.js
+import ReportForm from './ReportForm';
 
 const MockApp = () => {
   return (
-    // <BrowserRouter>
       <ReportForm>
         <div data-testid="app-content">Welcome to the Landslide App!</div>
       </ReportForm>
-    // </BrowserRouter>
   );
 };
-
 
 describe('ReportForm component', () => {
   test('renders Landslide Report Login when not authenticated', () => {
